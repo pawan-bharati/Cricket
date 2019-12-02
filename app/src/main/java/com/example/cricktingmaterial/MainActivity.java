a587bebc28c7481c8c71039b7055595d04e1ec37
pawan_bharati <pawanbharati90@gmail.com>
2019-12-02 11:58:22 +0545

    Login completed

--- app/src/main/java/com/example/cricktingmaterial/MainActivity.java
+++ app/src/main/java/com/example/cricktingmaterial/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.cricktingmaterial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+EditText etusername,etpassword;
+Button btnlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etusername=findViewById(R.id.etusername);
+        etpassword=findViewById(R.id.etpassword);
+        btnlogin=findViewById(R.id.btnlogin);
+
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (etusername.getText().toString().equals("admin")&&
+                        etpassword.getText().toString().equals("admin"))
+                {
+                    Intent intent =new Intent(MainActivity.this,Dashboard.class);
+                    startActivity(intent);
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Incorrect username or password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 }
