bad886a90afc71eadac028615e5888a77e7befde
pawan_bharati <pawanbharati90@gmail.com>
2020-01-13 11:36:10 +0545

    signup form

---
+++ app/src/main/java/com/example/cricktingmaterial/model/SigUpActivity.java
@@ -0,0 +1,16 @@
+package com.example.cricktingmaterial.model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cricktingmaterial.R;
+
+public class SigUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sig_up);
+    }
+}
