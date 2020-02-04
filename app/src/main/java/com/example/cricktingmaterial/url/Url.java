6f6ca0269cec160e7e1b334784d1bc99fc4af129
pawan <pawanbharati90@gmail.com>
2020-02-04 14:22:58 +0545

    layout changed

--- app/src/main/java/com/example/cricktingmaterial/url/Url.java
+++ app/src/main/java/com/example/cricktingmaterial/url/Url.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Url {
 
-    public static final String base_url="http://10.0.2.2:4000/";
+    public static final String base_url="http://10.0.2.2:5000/";
 public static  String imagepath = base_url + "uploads/" ;
     public static String token="Bearer ";
 
